12b6d89ec2b829fd4c5d7a8c2d3a935826200944
Parth Ubhe <64827269+parthubhe@users.noreply.github.com>
2025-04-11 14:11:53 +0530

    Update Calculator.java
    Added functions for addition and subtraction

--- lab_07_InbuiltExceptions/Calculator.java
+++ lab_07_InbuiltExceptions/Calculator.java
@@ -1,43 +1,27 @@
 public class Calculator {
 
-
+    /**
+     * Adds two numbers.
+     *
+     * @param a first number
+     * @param b second number
+     * @return sum of a and b
+     */
     public static double add(double a, double b) {
         return a + b;
     }
 
-
+    /**
+     * Subtracts second number from the first.
+     *
+     * @param a first number
+     * @param b second number
+     * @return difference of a and b
+     */
     public static double subtract(double a, double b) {
         return a - b;
     }
 
+ 
 
-    public static double multiply(double a, double b) {
-        return a * b;
-    }
-
-
-    public static double divide(double a, double b) throws ArithmeticException {
-        if (b == 0) {
-            throw new ArithmeticException("Cannot divide by zero");
-        }
-        return a / b;
-    }
-
-
-    public static double square(double a) {
-        return a * a;
-    }
-
-
-    public static double cube(double a) {
-        return a * a * a;
-    }
-
-
-    public static double squareRoot(double a) throws ArithmeticException {
-        if (a < 0) {
-            throw new ArithmeticException("Cannot take square root of a negative number");
-        }
-        return Math.sqrt(a);
-    }
 }
