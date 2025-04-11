16e1e69bea267db309b362ebe3b5a695659aa89a
Parth Ubhe <64827269+parthubhe@users.noreply.github.com>
2025-04-11 14:12:39 +0530

    Added functions for multiplication and division

--- lab_07_InbuiltExceptions/Calculator.java
+++ lab_07_InbuiltExceptions/Calculator.java
@@ -22,6 +22,31 @@ public class Calculator {
         return a - b;
     }
 
+    /**
+     * Multiplies two numbers.
+     *
+     * @param a first number
+     * @param b second number
+     * @return product of a and b
+     */
+    public static double multiply(double a, double b) {
+        return a * b;
+    }
+
+    /**
+     * Divides first number by second number.
+     *
+     * @param a dividend
+     * @param b divisor
+     * @return result of a / b
+     * @throws ArithmeticException if b is zero
+     */
+    public static double divide(double a, double b) throws ArithmeticException {
+        if (b == 0) {
+            throw new ArithmeticException("Cannot divide by zero");
+        }
+        return a / b;
+    }
  
 
 }
